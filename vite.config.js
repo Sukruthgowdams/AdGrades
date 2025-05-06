b74a6bec10aada24a979c3d78b526d13c9eb9616
Sukruth MS <sukruthdarks@gmail.com>
2025-05-07 02:03:24 +0530

    Deploy v1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import react from "@vitejs/plugin-react"
 
 // Replace 'your-repo-name' with your actual GitHub repository name
 export default defineConfig({
-  base: "/AdGrades/",
   plugins: [react(), tailwindcss()],
 })
